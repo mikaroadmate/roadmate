dac54f3dc86d36456a3c1ac46b2b5d5a32f29ed4
mikaroadmate <beaudeau_mickael@live.fr>
2026-04-17 05:51:23 +0200

    Fix sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', () => self.clients.claim())
+
 self.addEventListener('push', function(event) {
   const data = event.data ? event.data.json() : {}
   const title = data.title || 'RoadMate'
