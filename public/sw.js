2eb61fe6e27f39cddc62759deb3e61455a88ce91
mikaroadmate <beaudeau_mickael@live.fr>
2026-04-18 06:36:09 +0200

    PWA : suppression cache corrompu

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,16 @@
-const CACHE_NAME = 'roadmate-v2'
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/favicon.svg',
-  '/manifest.json'
-]
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  )
-  self.skipWaiting()
-})
+self.addEventListener('install', () => self.skipWaiting())
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   )
   self.clients.claim()
 })
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).catch(() => caches.match('/index.html'))
-    })
-  )
+self.addEventListener('message', (event) => {
+  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting()
 })
 
 self.addEventListener('push', function(event) {
@@ -47,7 +28,4 @@ self.addEventListener('push', function(event) {
 self.addEventListener('notificationclick', function(event) {
   event.notification.close()
   event.waitUntil(clients.openWindow(event.notification.data))
-})
-self.addEventListener('message', (event) => {
-  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting()
 })
